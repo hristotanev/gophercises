12b2407a93cfc18959440f51f035e233ab5a05d0
hristotanev <hristotanev7@gmail.com>
2022-03-23 18:42:59 +0000

    first part of first exercise done

--- quiz/quiz.go
+++ quiz/quiz.go
@@ -2,23 +2,19 @@ package main
 
 import (
 	"encoding/csv"
+	"flag"
 	"fmt"
 	"io"
 	"io/ioutil"
 	"strings"
 )
 
-func main() {
-  file, err := ioutil.ReadFile("problems.csv")
-  if err != nil {
-    panic("input file does not exist")
-  }
+func part1(fileContents string) {
+  questions, correctAnswers := 0, 0
+  csvReader := csv.NewReader(strings.NewReader(fileContents))
 
-  questions, correct := 0, 0
-  contents := string([]byte(file))
-  reader := csv.NewReader(strings.NewReader(contents))
   for {
-    line, err := reader.Read()
+    line, err := csvReader.Read()
     if err == io.EOF {
       break
     }
@@ -28,16 +24,29 @@ func main() {
     }
 
     fmt.Print("How much is ", line[0], ": ")
-    var answer string
-    fmt.Scan(&answer)
+    var usersAnswer string
+    fmt.Scan(&usersAnswer)
 
-    if line[1] == answer {
-      correct++
+    if line[1] == usersAnswer {
+      correctAnswers++
     }
 
     questions++
   }
 
   fmt.Println("Total number of questions", questions)
-  fmt.Println("Questions answered correctly", correct)
+  fmt.Println("Questions answered correctly", correctAnswers)
+}
+
+func main() {
+  filePath := flag.String("f", "./problems.csv", "file path of problems")
+  flag.Parse()
+
+  file, err := ioutil.ReadFile(*filePath)
+  if err != nil {
+    panic("input file does not exist")
+  }
+
+  contents := string([]byte(file))
+  part1(contents)
 }
