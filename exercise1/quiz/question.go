f811be502034cebfa6424ccb0fe89fd344ef1290
hristotanev <hristotanev7@gmail.com>
2022-05-13 14:32:39 +0100

    refactor exercise1

---
+++ exercise1/quiz/question.go
@@ -0,0 +1,10 @@
+package quiz
+
+type Question struct {
+	Question string
+	Answer   string
+}
+
+func (question *Question) EvaluateAnswer(usersAnswer string) bool {
+	return usersAnswer == question.Answer
+}
